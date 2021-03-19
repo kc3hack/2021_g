ccc144db6a7e8b0ccdd0df33716826c7525557b0
super <okc5424938d474w@gmail.com>
2021-03-19 15:38:57 +0900

    BASE64⇔画像処理追加

---
+++ front/smart_box/lib/Base64Utility/Base64Utility.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+
+///
+/// base64文字列からImageへ変換する
+///
+/// 例外が発生した場合はnull
+///
+Image base64ToImage(String aBase64String) {
+  Image aImage;
+  try {
+    Uint8List bytes = base64.decode(aBase64String);
+    aImage = Image.memory(bytes);
+  } catch (exception) {
+    print(exception);
+  }
+  return aImage;
+}
+
+///
+/// 画像からbase64文字列に変換する
+///
+/// 例外発生時は空文字列
+///
+String imageToBase64(File imageFile) {
+  String base64String;
+  try {
+    List<int> imageByte = imageFile.readAsBytesSync();
+    base64String = base64.encode(imageByte);
+  } catch (exception) {
+    print(exception);
+    return "";
+  }
+
+  return base64String;
+}
