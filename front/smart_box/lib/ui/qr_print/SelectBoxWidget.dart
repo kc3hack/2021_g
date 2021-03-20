6078c6a894c9a9a19b975c3676a7b98b0a6e2fbc
super <okc5424938d474w@gmail.com>
2021-03-21 03:25:49 +0900

    qr選択画面追加

--- front/smart_box/lib/ui/qr_print/SelectBoxWidget.dart
+++ front/smart_box/lib/ui/qr_print/SelectBoxWidget.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:smart_box/baggage/box.dart';
 import 'package:smart_box/server_interface/ServerInterface.dart';
 import 'package:smart_box/ui/qr_print/QrWidgetHolder.dart';
+import 'package:smart_box/ui/qr_print/SelectQrWidget.dart';
 
 class SelectBoxWidget extends StatefulWidget {
   final QrWidgetHolderState widgetHolderState;
@@ -13,6 +14,7 @@ class SelectBoxWidget extends StatefulWidget {
 
 class _SelectBoxWidgetState extends State<SelectBoxWidget> {
   List<Box> boxList = [];
+  int selected = -1;
 
   ///
   /// 表示するボックスを更新する
@@ -23,12 +25,16 @@ class _SelectBoxWidgetState extends State<SelectBoxWidget> {
     return;
   }
 
-  void _boxTapHandler(Box box) {}
+  void _boxTapHandler(int index, Box box) {
+    setState(() {
+      this.selected = index;
+    });
+  }
 
   ///
   /// 一覧に表示する1つのボックスを表すWidgetを作成する
   ///
-  Container _makeBoxTile(Box box) {
+  Container _makeBoxTile(int index, Box box) {
     return Container(
         color: Colors.white,
         margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
@@ -39,9 +45,11 @@ class _SelectBoxWidgetState extends State<SelectBoxWidget> {
             overflow: TextOverflow.ellipsis,
             style: TextStyle(fontSize: 16),
           ),
-          leading: Icon(Icons.check_box_outline_blank),
+          leading: (this.selected == index)
+              ? Icon(Icons.check_box_outlined)
+              : Icon(Icons.check_box_outline_blank),
           onTap: () {
-            _boxTapHandler(box);
+            _boxTapHandler(index, box);
           },
         ));
   }
@@ -61,6 +69,34 @@ class _SelectBoxWidgetState extends State<SelectBoxWidget> {
         child: Scaffold(
           appBar: AppBar(
             title: Text("QRコードプリント(1/3)"),
+          ),
+          floatingActionButton: Container(
+            width: 90,
+            height: 50,
+            child: Container(
+              //padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: (this.selected != -1)
+                    ? Color.fromARGB(255, 238, 152, 157)
+                    : Colors.grey,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: TextButton(
+                child: Text(
+                  "次へ",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                onPressed: () {
+                  if (this.selected == -1) {
+                    return;
+                  }
+                  widget.widgetHolderState
+                      .add(SelectQrWidget(widget.widgetHolderState));
+                },
+              ),
+            ),
           ),
           body: Column(
             children: [
@@ -90,7 +126,7 @@ class _SelectBoxWidgetState extends State<SelectBoxWidget> {
                         margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
                         child: Text("ボックスを選択してください",
                             style: TextStyle(
-                              fontSize: 16,
+                              fontSize: 15,
                             )),
                       )
                     ],
@@ -100,11 +136,17 @@ class _SelectBoxWidgetState extends State<SelectBoxWidget> {
               ///ボックスのリスト
               ///
               Flexible(
-                  child: ListView(
-                children: (this.boxList.isEmpty)
-                    ? [Container()]
-                    : this.boxList.map((box) => _makeBoxTile(box)).toList(),
-              )),
+                child: ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                    if (index == this.boxList.length)
+                      return Container(
+                        height: 90,
+                      );
+                    return this._makeBoxTile(index, this.boxList[index]);
+                  },
+                  itemCount: this.boxList.length + 1,
+                ),
+              ),
             ],
           ),
         ),
---
+++ front/smart_box/lib/ui/qr_print/SelectQrWidget.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:smart_box/ui/qr_print/QrWidgetHolder.dart';
+
+enum PrintType { BIG, MIDDLE, SMALL, STRIP }
+
+class SelectQrWidget extends StatefulWidget {
+  final QrWidgetHolderState widgetHolderState;
+  SelectQrWidget(this.widgetHolderState);
+  @override
+  _SelectQrWidgetState createState() => _SelectQrWidgetState();
+}
+
+class _SelectQrWidgetState extends State<SelectQrWidget> {
+  PrintType _type;
+
+  void _handleRadio(PrintType type) => setState(() {
+        _type = type;
+      });
+  Widget makeQrTile(Image image, PrintType type) {
+    String size;
+    String number;
+    switch (type) {
+      case PrintType.BIG:
+        size = "大サイズ";
+        number = "QRコード×4";
+        break;
+      case PrintType.MIDDLE:
+        size = "中サイズ";
+        number = "QRコード×6";
+        break;
+      case PrintType.SMALL:
+        size = "小サイズ";
+        number = "QRコード×8";
+        break;
+      case PrintType.STRIP:
+        size = "短冊サイズ";
+        number = "短冊型×4";
+        break;
+    }
+    return GestureDetector(
+        onTap: () {
+          _handleRadio(type);
+        },
+        child: Container(
+          padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            new Radio(
+              activeColor: Colors.grey,
+              value: type,
+              groupValue: _type,
+              onChanged: _handleRadio,
+            ),
+            Container(
+              width: 150,
+              height: 100,
+              child: image,
+            ),
+            Column(
+              children: [Text(size), Text(number)],
+            )
+          ]),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text("QRコードプリント(2/3)"),
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back_ios),
+                onPressed: widget.widgetHolderState.onWillPopHandler,
+                color: Theme.of(context).accentColor,
+              ),
+            ),
+            floatingActionButton: Container(
+              width: 90,
+              height: 50,
+              child: Container(
+                //padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: (this._type != null)
+                      ? Color.fromARGB(255, 238, 152, 157)
+                      : Colors.grey,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: TextButton(
+                  child: Text(
+                    "保存",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {
+                    if (this._type == null) {
+                      return;
+                    }
+                    widget.widgetHolderState.add(Container());
+                  },
+                ),
+              ),
+            ),
+            body: Column(children: [
+              ///
+              /// 説明欄
+              ///
+              Container(
+                  padding: EdgeInsets.fromLTRB(15, 15, 0, 5),
+                  child: Row(
+                    children: [
+                      ///
+                      /// stepアイコン
+                      ///
+                      Container(
+                        padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 247, 203, 206),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Text(
+                          "step2",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                        child: Text("QRコードを選択し保存してください",
+                            style: TextStyle(
+                              fontSize: 15,
+                            )),
+                      )
+                    ],
+                  )),
+              Flexible(
+                  child: ListView(
+                children: [
+                  this.makeQrTile(
+                      Image.asset("images/dummy.png"), PrintType.BIG),
+                  this.makeQrTile(
+                      Image.asset("images/dummy.png"), PrintType.MIDDLE),
+                  this.makeQrTile(
+                      Image.asset("images/dummy.png"), PrintType.SMALL),
+                  this.makeQrTile(
+                      Image.asset("images/dummy.png"), PrintType.STRIP),
+                  Container(
+                    height: 100,
+                  ),
+                ],
+              ))
+            ])),
+        onWillPop: widget.widgetHolderState.onWillPopHandler);
+  }
+}
